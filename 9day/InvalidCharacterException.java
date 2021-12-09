10d387cad737c728820ef4d91b4374ca3cf34db0
Tititino <59182246+Tititino@users.noreply.github.com>
2021-12-09 18:04:02 +0100

    Create InvalidCharacterException.java

---
+++ 9day/InvalidCharacterException.java
@@ -0,0 +1,9 @@
+public class InvalidCharacterException extends RuntimeException {
+	public InvalidCharacterException() {
+		super();
+	}
+
+	public InvalidCharacterException(String str) {
+		super(str);
+	}
+}
