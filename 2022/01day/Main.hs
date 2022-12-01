2d20f172049c96f0aa75c9365f430b8ce934fcb8
tino <mod01@outlook.it>
2022-12-01 14:17:55 +0100

    solution to day01

---
+++ 2022/01day/Main.hs
@@ -0,0 +1,10 @@
+import Control.Monad
+import System.Environment (getArgs)
+import Data.List (sort, groupBy)
+
+main :: IO ()
+main = do
+        cals <- longFunc . lines <$> (getArgs >>= (readFile . head))
+        putStrLn . (++) "solution to 1: " . show . maximum $ cals
+        putStrLn . (++) "solution to 2: " . show . sum . take 3 . reverse . sort $ cals
+                where longFunc = map (sum . (map (\x -> read x :: Integer) . tail)) . groupBy (\_ b -> b /= "")
