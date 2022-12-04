523b1d8f4714c33b9f946f24bbf90e7326a5b1d4
tino <mod01@outlook.it>
2022-12-04 15:44:19 +0100

    final polishing

--- 2022/04day/Main.hs
+++ 2022/04day/Main.hs
@@ -1,10 +1,10 @@
-import System.Environment (getArgs)
+import System.Environment ( getArgs )
 import Data.Maybe
 
 type Assignment = (Integer, Integer)
 
-predOnLines :: [String] -> (String -> Maybe a) -> (a -> Maybe Bool) -> [Bool]
-predOnLines line digestFunc predFunc = catMaybes (map (\x -> digestFunc x >>= predFunc) line)
+predOnLines :: [String] -> (String -> Maybe a) -> (a -> Bool) -> [Bool]
+predOnLines line digestFunc predFunc = catMaybes (map (\x -> digestFunc x >>= (return . predFunc)) line)
 
 digestAssignment :: String -> Maybe (Assignment, Assignment)
 digestAssignment s = do
@@ -13,12 +13,11 @@ digestAssignment s = do
                         (bs, (_:be)) <- return $ break (=='-') b
                         return ((read as :: Integer, read ae :: Integer), (read bs :: Integer, read be :: Integer))
 
-doCompletelyOverlap :: (Assignment, Assignment) -> Maybe Bool
-doCompletelyOverlap ((as, ae), (bs, be)) = return ((as <= bs && ae >= be) || (bs <= as && be >= ae))
+doCompletelyOverlap :: (Assignment, Assignment) -> Bool
+doCompletelyOverlap ((as, ae), (bs, be)) = ((as <= bs && ae >= be) || (bs <= as && be >= ae))
 
-doOverlap :: (Assignment, Assignment) -> Maybe Bool
-doOverlap ((as, ae), (bs, be)) = return ((as <= bs && ae >= bs) || (bs <= as && be >= as))
-
+doOverlap :: (Assignment, Assignment) -> Bool
+doOverlap ((as, ae), (bs, be)) = ((as <= bs && ae >= bs) || (bs <= as && be >= as))
 
 main :: IO ()
 main = do
