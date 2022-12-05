31442d225138e68e1919cf80660eb0c869ba46dd
tino <mod01@outlook.it>
2022-12-06 00:27:48 +0100

    sacrificed the maybe monad for the state monad

--- 2022/05day/Main.hs
+++ 2022/05day/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import System.Environment (getArgs)
 import Data.List
 import Control.Monad
+import qualified Control.Monad.State.Lazy as State
 import Data.Maybe
 import Data.Char (isDigit)
 import qualified Data.Map as Map
@@ -21,7 +22,7 @@ instance Show Crate where
 
 newtype Arrangement = Arrangement (Integer, Integer, Integer) deriving (Show, Eq, Ord)
 
--- slow but cooler version
+-- slower but cooler version
 -- instance Read Arrangement where
 --         readsPrec _ s =
 --                 let test = s ?=~ re in
@@ -30,12 +31,14 @@ newtype Arrangement = Arrangement (Integer, Integer, Integer) deriving (Show, Eq
 --                         [(Arrangement (read (matches !! 1) :: Integer, read (matches !! 2) :: Integer, read (matches !! 3) :: Integer), drop (length . head $ matches) s)]
 --                 else []
 --                 where re = fromJust $ compileRegex "^move ([0-9]+) from ([0-9]+) to ([0-9]+)"
+
+-- it does its job
 instance Read Arrangement where
         readsPrec _ s = case maybe of
                         Just arr -> arr
                         Nothing  -> []
                 where getNum s = 
-                        let str = dropWhile (not . isDigit) s in
+                        let str         = dropWhile (not . isDigit) s in
                         let (num, rest) = break (not . isDigit) str in
                         case num of
                         "" -> Nothing
@@ -45,7 +48,6 @@ instance Read Arrangement where
                         (num2, rest2) <- getNum rest1
                         (num3, rest3) <- getNum rest2
                         return [(Arrangement (num1, num2, num3), rest3)]
-
 
 type Stack = [Crate]
 
@@ -64,22 +66,21 @@ getCrates s =
         fst . foldl (\(map, ix) x -> (Map.insert ix x map, ix + 1)) (Map.empty, 1) $ stacks
         where prelude = takeWhile (not . isSeparatorString) s
 
-updateStacks :: Arrangement -> ([Crate] -> [Crate]) -> Stacks -> Maybe Stacks
-updateStacks (Arrangement (num, from, to)) f s = do
-        (str, fromStack) <- Map.lookup from s >>= return . splitAt numI
-        toStack          <- Map.lookup to s
-        return $ Map.insert to ((f str) ++ toStack) (Map.insert from fromStack s)
-        where numI  = fromIntegral num
-              fromI = fromIntegral from
-              toI   = fromIntegral to
+-- not using maybes may be bad
+updateStacks :: Arrangement -> ([Crate] -> [Crate]) -> State.State Stacks ()
+updateStacks (Arrangement (num, from, to)) f = do
+        ss <- State.get
+        let toMove = take (fromIntegral num) $ ss Map.! from
+        State.modify (Map.adjust (drop (fromIntegral num)) from)
+        State.modify (Map.adjust ((f toMove) ++) to)
+
 
 main :: IO ()
 main = do
         fileLines <- getArgs >>= readFile . head >>= return . lines
-        let stacks = getCrates . init $ fileLines 
+        let stacks       = getCrates . init $ fileLines 
         let arrangements = catMaybes . map (\x -> readMaybe x :: Maybe Arrangement) $ dropWhile (not . isArrangementString) fileLines 
-        -- from here on things get a little bad (unchecked fromJust, heads)
-        let res  = fromJust $ foldM (\acc arr -> updateStacks arr reverse acc) stacks arrangements
-        let res' = fromJust $ foldM (\acc arr -> updateStacks arr id acc) stacks arrangements
-        putStrLn . show . map snd . Map.toList . Map.map head $ res
-        putStrLn . show . map snd . Map.toList . Map.map head $ res'
+        let res          = State.execState $ forM arrangements (\arr -> updateStacks arr reverse)
+        let res'         = State.execState $ forM arrangements (\arr -> updateStacks arr id)
+        putStrLn . show . map snd . Map.toList . Map.map head $ res stacks
+        putStrLn . show . map snd . Map.toList . Map.map head $ res' stacks
