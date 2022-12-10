8bda40ff144565a426077b35802e3aa34f74f2b0
tino <mod01@outlook.it>
2022-12-10 19:23:03 +0100

    refactored day 9 condition

--- 2022/09day/Main.hs
+++ 2022/09day/Main.hs
@@ -16,16 +16,11 @@ updateHd L (x, y) = (x - 1, y)
 updateHd R (x, y) = (x + 1, y)
 
 updateTl :: (Int, Int) -> (Int, Int) -> (Int, Int)
-updateTl (hx, hy) (tx, ty)
-        | hx == tx && (hy == ty + 2)                               = (tx, ty + 1)
-        | hx == tx && (hy == ty - 2)                               = (tx, ty - 1)
-        | hy == ty && (hx == tx + 2)                               = (tx + 1, ty)
-        | hy == ty && (hx == tx - 2)                               = (tx - 1, ty)
-        | (hx > tx && (hy > ty + 1)) || ((hx > tx + 1) && hy > ty) = (tx + 1, ty + 1)
-        | (hx < tx && (hy > ty + 1)) || ((hx < tx - 1) && hy > ty) = (tx - 1, ty + 1)
-        | (hx > tx && (hy < ty - 1)) || ((hx > tx + 1) && hy < ty) = (tx + 1, ty - 1)
-        | (hx < tx && (hy < ty - 1)) || ((hx < tx - 1) && hy < ty) = (tx - 1, ty - 1)
-        | otherwise                                                = (tx, ty)
+updateTl (hx, hy) (tx, ty) = if (abs x) > 1 || (abs y) > 1
+                             then (tx + (signum x), ty + (signum y))
+                             else (tx, ty)
+                             where x = hx - tx
+                                   y = hy - ty
 
 mapMove :: (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
 mapMove _ [] = []
@@ -47,14 +42,14 @@ move (dir:rest) = do
 
 digestLines :: [[String]] -> [Direction]
 digestLines [] = []
-digestLines (["U", num]:rest) = (replicate ( read num :: Int) U) ++ (digestLines rest)
-digestLines (["D", num]:rest) = (replicate ( read num :: Int) D) ++ (digestLines rest)
-digestLines (["L", num]:rest) = (replicate ( read num :: Int) L) ++ (digestLines rest)
-digestLines (["R", num]:rest) = (replicate ( read num :: Int) R) ++ (digestLines rest)
+digestLines (["U", num]:rest) = (replicate (read num :: Int) U) ++ (digestLines rest)
+digestLines (["D", num]:rest) = (replicate (read num :: Int) D) ++ (digestLines rest)
+digestLines (["L", num]:rest) = (replicate (read num :: Int) L) ++ (digestLines rest)
+digestLines (["R", num]:rest) = (replicate (read num :: Int) R) ++ (digestLines rest)
 
 main :: IO ()
 main = do
         fileLines <- getArgs >>= (readFile . head) >>= return . map words . lines
         let directions = digestLines fileLines
-        forM_ [3, 10] (\x -> putStrLn . show $ State.evalState (move directions) (replicate x (0, 0), Set.empty))
+        forM_ [2, 10] (\x -> putStrLn . show $ State.evalState (move directions) (replicate x (0, 0), Set.empty))
 
